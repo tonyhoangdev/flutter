f46b8669b9ae552339a35c440aea7f95c927cb90
Tony Hoang <hoangtrongminh304@gmail.com>
2019-01-16 15:45:00 +0700

    update 2

--- 01-Hello/myapp/lib/main.dart
+++ 01-Hello/myapp/lib/main.dart
@@ -2,46 +2,79 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
-
 class MyApp extends StatelessWidget {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32.0),
-    child: Row(
+    // 
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold
-                )
-              ),
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color
             ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              )
-            )
-          ],
-        ),
-      ),
-      Icon (
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      Text('41')
-    ])
-  );
+          ),
+        )
+      ],
+    );
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      )
+    );
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                )
+              )
+            ],
+          ),
+        ),
+        Icon (
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41')
+      ])
+    ); 
+
     return MaterialApp(
       title: 'Flutter Layout Demo',
       home: Scaffold(
@@ -49,9 +82,13 @@ class MyApp extends StatelessWidget {
         title: Text('Top Lakes')
         ),
         body: Column(
-          children: [titleSection,],
+          children: [
+            titleSection,
+            buttonSection
+            ],
           )
       )
     );
+
   }
 }
