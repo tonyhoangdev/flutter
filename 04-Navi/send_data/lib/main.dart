1d7968d7ff0bc4750576861256294e6d312b36f8
Tony Hoang <hoangtrongminh304@gmail.com>
2019-01-22 23:40:03 +0700

    Add send data to a new screen

---
+++ 04-Navi/send_data/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: TodosScreen(
+        todos: List.generate(
+          20, 
+          (i) => Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          )),
+      )
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                )
+              );
+            },
+          );
+        },
+      )
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Todo todo;
+
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${todo.title}"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text("${todo.description}"),
+      )
+    );
+  }
+}
